481da5a0ec3da25b6f58f084a284d74b4f761516
LestathRiveraD <cesarrivera0404@gmail.com>
2026-04-24 23:00:35 -0700

    main.java

--- src/calendarizador/Main.java
+++ src/calendarizador/Main.java
@@ -1,15 +1,25 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import modelo.PCB;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        /*
+        TODO:
+        Manejar valores negativos o cero en tiempo de rafaga
+        pids duplicados
+        lista de procesos vacia
+        archivo de entrada con formato incorrecto
+        */
         // Obtener los datos
         List<String> lineas = new ArrayList<>();
+        List<PCB> procesos = new ArrayList<>();
         if (args.length == 1) // Leer txt con los datos
         {
+            // Leer lineas del archivo proporcionado por el usuario
             try {
                 lineas = Files.readAllLines(Paths.get(args[0]));
             } 
@@ -17,12 +27,30 @@ public class Main
                 System.out.println("Lectura de " + args[0] + " falló");
                 return;
             }
-            
-            for (String linea : lineas)
+                
+            // Convertir las lineas del archivo de texto a objetos PCB
+            int pid, llegada, rafaga, prioridad, tiempoIO;
+            String nombre;
+            for (int i = 1; i < lineas.size(); i++) 
             {
-                String[] proceso = linea.split(","); 
-                if ()
+                String[] cur = lineas.get(i).split(",");
+
+                if (cur.length < 6) {
+                    System.out.println("Linea invalida");
+                    continue;
+                }
+
+                PCB p = new PCB(
+                    Integer.parseInt(cur[0]),
+                    cur[1],
+                    Integer.parseInt(cur[2]),
+                    Integer.parseInt(cur[3])
+                );
+        
+                procesos.add(p);
             }
+
+            System.out.println(procesos);
         }
         else // Leer datos por consola
         {
