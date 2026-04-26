6ab3ac948a45d6e604e2e039839c72744019df23
EmmmanuelRCPP <164968372+EmmmanuelRCPP@users.noreply.github.com>
2026-04-26 16:07:33 -0700

    clase simulador iteracion de queues

--- src/calendarizador/simulacion/GestorColas.java
+++ src/calendarizador/simulacion/GestorColas.java
@@ -6,11 +6,53 @@ import modelo.PCB;
 
 public class GestorColas(String[] args)
 {
-    Queue<PCB> nuevos = new LinkedList<PCB>();
-    Queue<PCB> listos = new LinkedList<PCB>();
-    Queue<PCB> bloqueados = new LinkedList<PCB>();
-    Queue<PCB> terminados = new LinkedList<PCB>();
-    PCB procesoActual = null;
+    private Queue<PCB> nuevos = new LinkedList<PCB>();
+    private Queue<PCB> listos = new LinkedList<PCB>();
+    private Queue<PCB> bloqueados = new LinkedList<PCB>();
+    private Queue<PCB> terminados = new LinkedList<PCB>();
+    private PCB procesoActual = null;
+
+    // ===== GETTERS =====
+    public Queue<PCB> getNuevos() {
+        return nuevos;
+    }
+
+    public Queue<PCB> getListos() {
+        return listos;
+    }
+
+    public Queue<PCB> getBloqueados() {
+        return bloqueados;
+    }
+
+    public Queue<PCB> getTerminados() {
+        return terminados;
+    }
+
+    public PCB getProcesoActual() {
+        return procesoActual;
+    }
+
+    // ===== SETTERS =====
+    public void setNuevos(Queue<PCB> nuevos) {
+        this.nuevos = nuevos;
+    }
+
+    public void setListos(Queue<PCB> listos) {
+        this.listos = listos;
+    }
+
+    public void setBloqueados(Queue<PCB> bloqueados) {
+        this.bloqueados = bloqueados;
+    }
+
+    public void setTerminados(Queue<PCB> terminados) {
+        this.terminados = terminados;
+    }
+
+    public void setProcesoActual(PCB procesoActual) {
+        this.procesoActual = procesoActual;
+    }
 
     @Override
     public String toString() {
--- src/calendarizador/simulacion/Simulador.java
+++ src/calendarizador/simulacion/Simulador.java
@@ -7,15 +7,22 @@ public class Simulador implements Calendarizador
 {
     private Calendarizador calendarizador;
     private GestorColas gestorColas
+    private current_tick;
     public Simulador(Calendarizador calendarizador, GestorColas gestorColas)
     {
         this.calendarizador = calendarizador;
         this.gestorColas = gestorColas;
+        current_tick = 0;
     }
 
     public void tick()
     {
-
+        //Verificar si llegan nuevos procesos y admitirlos en la cola de listo
+        private Queue<PCB> nuevos = gestorColas.getNuevos();
+        Iterator<PCB> iterator = pq.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
     }
 
 }
