3fc438552f69a553e0aacee2a5b1888cbf58a9ec
Jonathan Davis <jonathan.davis@cetys.edu.mx>
2026-04-26 17:17:14 -0800

    feat: corregir la clase que estaba declarada como metodo, cambiar los atributos a private y aniadir un metodo getProcesoActual()

--- src/calendarizador/simulacion/GestorColas.java
+++ src/calendarizador/simulacion/GestorColas.java
@@ -1,23 +1,33 @@
 package simulacion;
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import modelo.PCB;
 
-public class GestorColas(String[] args)
+public class GestorColas
 {
-    Queue<PCB> nuevos = new LinkedList<PCB>();
-    Queue<PCB> listos = new LinkedList<PCB>();
-    Queue<PCB> bloqueados = new LinkedList<PCB>();
-    Queue<PCB> terminados = new LinkedList<PCB>();
-    PCB procesoActual = null;
+    private Queue<PCB> nuevos = new LinkedList<PCB>();
+    private Queue<PCB> listos = new LinkedList<PCB>();
+    private Queue<PCB> bloqueados = new LinkedList<PCB>();
+    private Queue<PCB> terminados = new LinkedList<PCB>();
+    private PCB procesoActual = null;
+
+    // Metodo para el GanttRenderer
+    public  PCB getProcesoActual() {
+        return procesoActual;
+    }
+
+    public void setProcesoActual(PCB proceso) {
+        this.processoActual = proceso;
+    }
 
     @Override
     public String toString() {
     return      "  Estado      | PID\n" + 
-                "  Nuevos      | " + Nuevos + "\n" +
-                "  Listos      | " + Listos + "\n" +
-                "  Bloqueados  | " + Bloqueados + "\n" +
-                "  Terminados  | " + Terminados + "\n";
+                "  Nuevos      | " + nuevos + "\n" +
+                "  Listos      | " + listos + "\n" +
+                "  Bloqueados  | " + bloqueados + "\n" +
+                "  Terminados  | " + terminados + "\n";
     }
 } 
