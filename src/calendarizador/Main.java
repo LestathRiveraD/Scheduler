2433e29382b7a910b9b462bbdbb9c301d331d609
LestathRiveraD <cesarrivera0404@gmail.com>
2026-04-26 18:31:21 -0700

    feat: handling pids duplicadas y add mensajes de error

--- src/calendarizador/Main.java
+++ src/calendarizador/Main.java
@@ -7,16 +7,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        /*
-        TODO:
-        Manejar valores negativos o cero en tiempo de rafaga
-        pids duplicados
-        lista de procesos vacia
-        archivo de entrada con formato incorrecto
-        */
-        // Obtener los datos
         List<String> lineas = new ArrayList<>();
         List<PCB> procesos = new ArrayList<>();
+
         if (args.length == 1) // Leer txt con los datos
         {
             // Leer lineas del archivo proporcionado por el usuario
@@ -27,34 +20,78 @@ public class Main
                 System.out.println("Lectura de " + args[0] + " falló");
                 return;
             }
-                
+
             // Convertir las lineas del archivo de texto a objetos PCB
-            int pid, llegada, rafaga, prioridad, tiempoIO;
-            String nombre;
             for (int i = 1; i < lineas.size(); i++) 
             {
                 String[] cur = lineas.get(i).split(",");
 
-                if (cur.length < 6) {
+                if (cur.length < 4) {
                     System.out.println("Linea invalida");
                     continue;
                 }
 
                 PCB p = new PCB(
-                    Integer.parseInt(cur[0]),
-                    cur[1],
-                    Integer.parseInt(cur[2]),
-                    Integer.parseInt(cur[3])
+                    Integer.parseInt(cur[0].trim()),
+                    cur[1].trim(),
+                    Integer.parseInt(cur[2].trim()),
+                    Integer.parseInt(cur[3].trim())
                 );
-        
+
                 procesos.add(p);
             }
-
-            System.out.println(procesos);
         }
-        else // Leer datos por consola
+        else if (args.length == 0) // Leer datos por consola
         {
+            Scanner sc = new Scanner(System.in);
 
+            System.out.print("¿Cuántos procesos desea ingresar?: ");
+            int n = sc.nextInt();
+            sc.nextLine(); // limpiar buffer
+
+            for (int i = 0; i < n; i++)
+            {
+                System.out.println("\nProceso " + (i + 1));
+
+                System.out.print("PID: ");
+                int pid = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("Nombre: ");
+                String nombre = sc.nextLine();
+
+                System.out.print("Tiempo de llegada: ");
+                int llegada = sc.nextInt();
+
+                System.out.print("Ráfaga CPU: ");
+                int rafaga = sc.nextInt();
+                sc.nextLine();
+
+                PCB p = new PCB(pid, nombre, llegada, rafaga);
+                procesos.add(p);
+            }
+            sc.close();
+        }
+        else
+        {
+            System.out.println("Uso: Java Main [nombre_archivo.txt] / Java Main");
+        }
+
+        Set<Integer> s = new HashSet<>();
+        List<Integer> l = new ArrayList<>(); 
+
+        for (PCB p : procesos)
+        {
+            s.add(p.getPid());
+            l.add(p.getPid());
+        }
+
+            System.out.println(s.size());
+            System.out.println(l.size());
+        if (s.size() != l.size())
+        {
+            System.out.println("Error: PIDS duplicadas.");
+            return;
         }
     }
 }
