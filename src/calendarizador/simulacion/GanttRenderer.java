dc842324d3fa1822eda5991d7de0ee618f81a15a
Jonathan Davis <jonathan.davis@cetys.edu.mx>
2026-04-26 16:08:25 -0800

    feat: implementacion de la estructura para el diagrama de Gantt

--- src/calendarizador/simulacion/GanttRenderer.java
+++ src/calendarizador/simulacion/GanttRenderer.java
@@ -1,1 +1,42 @@
 package simulacion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GanttRenderer {
+    // Lista para guardar el nombre o PID del proceso ejecutado en cada segundo
+    private List<String> historialEjecucion;
+
+    public GanttRenderer() {
+        this.historialEjecucion = new ArrayList<>();
+    }
+
+    // Registra que proceso ocupo la CPU en un tick especifico
+    // Si no hay proceso, se puede pasar NULL
+    public void registrarTick(String nombreProceso) {
+        if (nombreProceso == null) {
+            historialEjecucion.add("--") // Representacion de CPU ociosa
+        } else {
+            historialEjecucion.add(nombreProceso);
+        }
+    }
+
+    // Imprime el diagrama de Gantt en consola al finalizar la simulacion
+    public void mostrarDiagrama() {
+        System.out.println("\n--- Diagrama de Gantt ---");
+
+        // Imprime fila de tiempos
+        System.out.print("t= ");
+        for (int i = 0; i < historialEjecucion.size(); i++) {
+            System.out.printf("%-3d", i);
+        }
+        System.out.print();
+
+        // Imprime fila de procesos
+        System.out.print("  ");
+        for (String p : historialEjecucion) {
+            System.out.printf("%-3s", p);
+        }
+        System.out.println("\n")
+    }
+}
